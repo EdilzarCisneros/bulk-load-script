3a5c6f9f0051ad2da7ee18c3b16303d4312ab293
Edilzar Cisneros <Edilzar.Cisneros@base22.com>
2021-06-14 13:17:36 -0500

    Feature: Add help text and fotmat

--- src/startBulkLoad.sh
+++ src/startBulkLoad.sh
@@ -1,11 +1,25 @@
 #/bin/bash
 
+echo "++++++++Bulk Load Command Generator++++++++"
+echo " "
+echo "The full .tar file name (including the extension .tar)"
 read -p "Enter File Name: " fileName
+echo " "
+echo "Reports folders:"
+echo " "
+echo "PFP = PFP"
+echo "Commitment Summary = CommitmentSummary"
+echo "First Pass = FirstPass"
+echo "OEA - Customer Group Summary = OEACustGrp"
+echo "OEA Location = OEALocation"
+echo "OEA Worksheet = OEAWorksheet"
+echo " "
 read -p "Enter Folder Destination: " pathDest
 read -p "Is it for the new portal (y/n): " -n 1 isNew
-echo "\n\n"
+echo " "
 
-echo "================================================"
+echo "=====================Commands==========================="
+echo " "
 if [[ -n $fileName ]]; then
   if [ "$isNew" = "y" ]; then
     echo "ssh f07540c@uxhazpa01p.casecorp.com"
@@ -27,4 +41,16 @@ if [[ -n $fileName ]]; then
   echo "cd $scriptDir"
   echo "$scriptName"
 fi
-echo "================================================"
+echo " "
+echo "=====================Additional Instructions==========================="
+echo " "
+echo "Please run the commands above one by one, and read the followowing indications"
+echo "1. Make sure the file name is the same as in uxhazem1"
+echo "2. Make sure the .tar file is copied in the proper directory"
+echo "3. Make sure the .xml file is a valid xml"
+echo "4. Make sure the the format in the .xml file is correct"
+echo "5. Make sure the there is no white space at the beginning in the .xml file"
+echo "6. Make sure the the categories are correct"
+echo "7. Make sure the .pdf files match with the ones described in the .xml file "
+echo "8. Once the bulk load ends, please look at the content in WCM"
+
